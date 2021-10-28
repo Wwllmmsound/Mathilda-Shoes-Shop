352cb6ca6edb5ebc5bcd426930e63765f8937744
WWLLMM <83589558+Wwllmmsound@users.noreply.github.com>
2021-10-28 16:44:00 +0200

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,20 @@
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (var i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function(){
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block")
+         {dropdownContent.style.display = "none";
+    } else {
+    dropdownContent.style.display = "block";
+    }
+  });
+}
+
+function articleWindow1() {
+  console.log("Je suis la coucou")
+  var popup = document.getElementById("popupbotte1");
+  popup.hidden=false;
+}
